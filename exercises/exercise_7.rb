2b02bf4efa963ed1f8d2b4b4475cf9abce16ad8c
Monica Li <monicali@live.ca>
2021-01-05 22:08:46 +0000

    displays error messsage when inputting an invalid store to database

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "What is the name of your store?"
+@name = gets.chomp
+new_store = Store.create(name: @name);
+puts new_store.valid?
+puts new_store.errors.full_messages
+puts new_store.name?
+# newStore.save!
+# puts newStore.errors
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,6 @@
 class Store  < ActiveRecord::Base
   has_many :employees
+
+  validates :name, :length => { :minimum => 3, message: "name is too short" }
+  validates :annual_revenue, :numericality => { :greater_than => 0,  message: "pls input a num greater than 0" }
 end
