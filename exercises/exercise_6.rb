026573f0fda96bbc421cb9d3861972b233dd1c4d
Monica Li <monicali@live.ca>
2021-01-05 04:00:11 +0000

    adds exercise6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Monica", last_name: "Monica", hourly_rate: 61)
+@store1.employees.create(first_name: "Pepper", last_name: "Peppo", hourly_rate: 63)
+
+@store2.employees.create(first_name: "Kshun", last_name: "Smol", hourly_rate: 64)
+@store2.employees.create(first_name: "Leslie", last_name: "Knope", hourly_rate: 67)
+@store2.employees.create(first_name: "Ron", last_name: "Swanson", hourly_rate: 61)
